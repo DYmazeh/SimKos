710c4ee6e8425694f9b0c13cdf606ba992f61a1d
DYmazeh <dymaz.satya2005@gmail.com>
2026-05-18 21:07:16 +0700

    Add Dockerfile with gd extension for Railway

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -33,9 +33,14 @@ class PasswordResetLinkController extends Controller
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+        try {
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
+        } catch (\Exception $e) {
+            // TEMPORARY LOGGING: Menampilkan error langsung di layar
+            dd('ERROR EMAIL:', $e->getMessage(), 'Stack Trace:', $e->getTraceAsString());
+        }
 
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
